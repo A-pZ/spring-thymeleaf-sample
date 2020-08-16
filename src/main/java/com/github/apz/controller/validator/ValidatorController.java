08a8f4e4c350a0ee5985bedd570de7a9f25904e4
A-pZ <g.alpha.pz@gmail.com>
2020-08-16 12:54:40 +0900

    不要なflash属性の格納などを削除

--- src/main/java/com/github/apz/controller/validator/ValidatorController.java
+++ src/main/java/com/github/apz/controller/validator/ValidatorController.java
@@ -1,20 +1,16 @@
 package com.github.apz.controller.validator;
 
-import java.util.ArrayList;
-
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -23,16 +19,14 @@ import lombok.Setter;
 @RequestMapping("/validator")
 public class ValidatorController {
 	@GetMapping
-	public ModelAndView display(ModelAndView mnv, @ModelAttribute("form") UserRegisterForm form, @ModelAttribute("errors") ArrayList<FieldError> errors) {
+	public ModelAndView display(ModelAndView mnv, @ModelAttribute("form") UserRegisterForm form) {
 		mnv.setViewName("validator");
 		return mnv;
 	}
 
 	@PostMapping
-	public ModelAndView check(@ModelAttribute("form") @Validated UserRegisterForm form, BindingResult result, ModelAndView mnv, RedirectAttributes flash) {
+	public ModelAndView check(@ModelAttribute("form") @Validated UserRegisterForm form, BindingResult result, ModelAndView mnv) {
 		if (result.hasErrors()) {
-			var errors = result.getFieldErrors();
-			flash.addFlashAttribute("errors", errors);
 			mnv.setViewName("validator");
 			return mnv;
 		}
